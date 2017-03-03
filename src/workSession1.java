ebb4f9cc74dfe1647834dbae05696c06d6423ca2
doooud <philippick.a@gmail.com>
2017-03-03 20:32:16 +0100

    Commit sur WorkSession1 : Final

--- src/workSession1.java
+++ src/workSession1.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 
-/**
- * Created by Andrew on 03/03/2017.
- */
-
 public class workSession1 {
 
+
     private static void ruler(int x, int y){
+
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Taille de votre règle :");
+        x = scan.nextInt();
+        System.out.println("Division toute les :");
+        y = scan.nextInt();
+
+
         int i;
 
         for(i=0; i<=x; i++){
@@ -19,6 +24,13 @@ public class workSession1 {
     }
 
     private static void power(int a, int n){
+
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Donnez une valeur :");
+        a = scan.nextInt();
+        System.out.println("Donnez une puissance :");
+        n = scan.nextInt();
+
 
         int i;
         int b = a;
@@ -34,23 +46,32 @@ public class workSession1 {
 
     private static int premier(int w){
 
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Donnez une valeur :");
+        w = scan.nextInt();
+
+
         int v = 1;
         int f = 0;
         int i;
-        int result = 0;
+        int result = 1;
 
         for(i=2; i<w; i++){
 
             if(w%i == 0){
                 result = f;
             }
-            else{
-                result = v;
-            }
         }
         
-        return result;
+        if(result == 1){
+            System.out.println("C'est un nombre premier");
+            return result;
 
+        }
+        else{
+            System.out.println("Ce n'est pas un nombre premier");
+            return result;
+        }
 
     }
 
@@ -73,6 +94,13 @@ public class workSession1 {
 
     private static void calculerTable(int  nb, int max){
 
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Combien de ligne (nb) :");
+        nb = scan.nextInt();
+        System.out.println("Combien de colonne (max) :");
+        max = scan.nextInt();
+        System.out.println("Voici votre table de multiplication");
+
         int j;
         int i;
 
@@ -86,25 +114,41 @@ public class workSession1 {
 
     }
 
-    private static void recherche(int val, int [] tab) {
+    private static void recherche(int val, int size) {
 
-        int length = tab.length;
+        Scanner scan = new Scanner (System.in);
+
+        System.out.println("Quelle valeur cherchez-vous ? :");
+        val = scan.nextInt();
+
+        System.out.println("Veuillez rentrer la taille de votre table :");
+        size = scan.nextInt();
+
+        int [] mytab  = new int[size];
+
+        int z;
+
+        for(z=0; z< mytab.length; z++){
+            System.out.print("Entrez un nombre dans votre table : ");
+            mytab[z] = scan.nextInt();
+        }
+
         int i;
         int f = 1;
         int index = -1;
 
-        for(i = 0; i < length; i++){
-            if (tab[i] == val){
+        for(i = 0; i < size; i++){
+            if (mytab[i] == val){
                 index = i;
             }
         }
 
 
         if(index < 0){
-            System.out.println(-f);
+            System.out.println("L'élément n'est pas dans votre liste :" + (-f));
         }
         else{
-            System.out.println(index);
+            System.out.println("L'élément est dans votre liste et son index est : " + index);
         }
 
     }
@@ -121,41 +165,34 @@ public class workSession1 {
         System.out.println("Rentrez 1 pour la Règle");
         System.out.println("Rentrez 2 pour la Puissance");
         System.out.println("Rentrez 3 pour les nombres Premiers");
-        System.out.println("Rentrez 4 pour la calculerTable (15*10)");
-        System.out.println("Rentrez 5 pour la Recherche");
+        System.out.println("Rentrez 4 pour la table demandée");
+        System.out.println("Rentrez 5 pour la calculerTable");
+        System.out.println("Rentrez 6 pour la Recherche");
 
 
         do{
             foo = scan.nextInt();
-        }while(foo != 1 && foo !=2 && foo!=3 && foo!=4 && foo!=5);
+        }while(foo != 1 && foo !=2 && foo!=3 && foo!=4 && foo!=5 && foo!=6);
 
 
         switch(foo){
             case 1:
-                ruler(53, 10);
+                ruler(0,0); //initialisation pour arguments
                 break;
             case 2:
-                power(16,2);
+                power(0,0);
                 break;
             case 3:
-                System.out.println(premier(15));
+                System.out.println(premier(0));
                 break;
             case 4:
                 table();
                 break;
             case 5:
-                calculerTable(15, 10);
+                calculerTable(0, 0);
                 break;
             case 6:
-                int [] tab1 = {1,2,3,4,5};
-                int [] tab2 = {1,2,3,4,5};
-                int [] tab3 = {9, 8, 7, 6,5,4,3,2,1,0};
-                int [] tab4 = {9, 8, 7, 6,5,4,3,2,1,0};
-
-                recherche(55,tab1);
-                recherche(5,tab2);
-                recherche(9,tab3);
-                recherche(5,tab4);
+                recherche(0,0);
                 break;
             default:
                 System.out.print("Je ne comprends pas votre requête");
